d2d163457c769f2d3c11e0fca3e419dd0bed6f60
Vincent <vao2002@gmail.com>
2021-08-12 12:34:08 -0500

    commit tree 50de20671eb985cef81a2794698358ed22ab3d29

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,19 +1,14 @@
 // Code EyesOnMe Component Here
 import React, {useState} from "react";
 
-let isOn;
 
-function handleFocus(event){
-    isOn=!isOn
-
-}
 
 function EyesOnMe(){
     return(
         <div>
             <button onFocus={()=>{console.log('Good!')}} 
                     onBlur={()=>{console.log('Hey! Eyes on me!')}}>
-                Hey! Eyes on me!
+                Eyes on me
             </button>
         </div>
         
